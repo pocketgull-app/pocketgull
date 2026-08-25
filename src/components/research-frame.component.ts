aadc4d0be354cdd6624cd63ea2a80f68faa040e7
Phil Gear <dpo@pocketgull.app>
2026-08-24 17:58:52 -0700

    fix(build): resolve component imports and clean compiler options

--- src/components/research-frame.component.ts
+++ src/components/research-frame.component.ts
@@ -34,7 +34,9 @@ export interface IPubMedSearchResult {
     PocketGullButtonComponent, 
     PocketGullInputComponent, 
     SafeHtmlPipe, 
-    PatientEducationFlipDirective
+    PatientEducationFlipDirective,
+    NcaaSportsScienceHubComponent,
+    InternationalUniversityHubComponent
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
