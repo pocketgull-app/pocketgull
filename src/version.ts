122e555c78614e102ec3a7a5ec481a4d951d457d
Phil Gear <dpo@pocketgull.app>
2026-09-17 22:07:39 -0700

    chore(release): bump version to v1.37.0 and update sbom
    Signed-off-by: Phil Gear <dpo@pocketgull.app>

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.30.0';
-export const APP_RELEASE_NAME = 'v1.30.0-cms-rpm-smart-expansion';
+export const APP_VERSION = '1.37.0';
+export const APP_RELEASE_NAME = 'v1.37.0-data-dividend-poka-yoke-app-orchard';
